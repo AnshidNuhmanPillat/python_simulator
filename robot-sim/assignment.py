62b88af351bd892003a4b0a4c00e8f6bf1c4a16a
Anshid <anshid.unige@gmail.com>
2022-11-16 13:01:12 +0000

    rectified the errors
    corrected and modified the code
    added comments

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -4,33 +4,24 @@ import time
 from sr.robot import *
 
 """
-Exercise 3 python script
-Put the main code after the definition of the functions. The code should make the robot:
-	- 1) find and grab the closest silver marker (token)
-	- 2) move the marker on the right
-	- 3) find and grab the closest golden marker (token)
-	- 4) move the marker on the right
-	- 5) start again from 1
-The method see() of the class Robot returns an object whose attribute info.marker_type may be MARKER_TOKEN_GOLD or MARKER_TOKEN_SILVER,
-depending of the type of marker (golden or silver). 
-Modify the code of the exercise2 to make the robot:
-1- retrieve the distance and the angle of the closest silver marker. If no silver marker is detected, the robot should rotate in order to find a marker.
-2- drive the robot towards the marker and grab it
-3- move the marker forward and on the right (when done, you can use the method release() of the class Robot in order to release the marker)
-4- retrieve the distance and the angle of the closest golden marker. If no golden marker is detected, the robot should rotate in order to find a marker.
-5- drive the robot towards the marker and grab it
-6- move the marker forward and on the right (when done, you can use the method release() of the class Robot in order to release the marker)
-7- start again from 1
-	When done, run with:
-	$ python run.py solutions/exercise3_solution.py
+Assignment 1 python script. 
+Functions defined:
+1. drive
+2. turn
+3. find_silver_token
+4. find_golden_token
+5. check_g_pair
+6. check_s_pair
+
+the main program will stop once all the tokens are in pair
 """
 
 
 a_th = 2.0
-""" float: Threshold for the control of the linear distance"""
+""" float: Threshold for the control of the orientation"""
 
 d_th = 0.4
-""" float: Threshold for the control of the orientation"""
+""" float: Threshold for the control of the linear distance"""
 
 silver = True
 """ boolean: variable for letting the robot know if it has to look for a silver or for a golden marker"""
@@ -98,81 +89,92 @@ def find_golden_token():
     else:
         return dist, rot_y
 
-
-"""
-function for grabbing the silver token
-"""
-""" 
-def robot_grab(d_th,a_th):
-    grabbed = -1 # variable to know whether the robot grabbed or not
-    pair_chk = 0 # variable for checking whether all the silver and golden tokens are in pairs
-    while 1:
-        dist_s, rot_y_s = find_silver_token()
-        dist_g, rot_y_g = find_golden_token()
-        if dist_s==-1: # if no silver token is detected, we make the robot turn
-            print("I don't see any token!!")
-            turn(+10, 1)
-        elif (dist_g!=-1 and (abs(dist_s-dist_g)<d_th and abs(rot_y_s-rot_y_g)<a_th)): # checking whether the silver token found is close to a golden tocken
-            print("The silver token is pair with a golden token, again checking")
-            turn(+10,1)
-            pair_chk+=1
-            
-        elif dist_s <d_th: # if we are close to the token, we try grab it.
-            print("Found it!")
-            if R.grab(): # if we grab the token, we move the robot forward and on the right, we release the token, and we go back to the initial position
-                print("Gotcha!")
-                grabbed = 1
+def check_g_pair(dist_g_m, d_th):
+    """
+    function to check whether the golden token in pursuit is in a pair with a silver token.
+    to do that, first the shortest distance between the golden token and the silver tokens are calculated and if the shortest distance is smaller than a threshold, then the token is in pair
+    """
+    dist = 100
+    diff_t = -1
+    i = 1
+    pair_gs = 0
+    for token in R.see():
+        if token.dist < dist and token.info.marker_type is MARKER_TOKEN_SILVER:
+            if i==1:
+                diff_t = abs(token.dist-dist_g_m)
             else:
-                print("Aww, I'm not close enough.")
-        elif -a_th<= rot_y_s <= a_th: # if the robot is well aligned with the token, we go forward
-            print("Ah, that'll do.")
-            drive(10, 0.5)
-        elif rot_y_s < -a_th: # if the robot is not well aligned with the token, we move it on the left or on the right
-            print("Left a bit...")
-            turn(-2, 0.5)
-        elif rot_y_s > a_th:
-            print("Right a bit...")
-            turn(+2, 0.5)
+                temp = abs(token.dist-dist_g_m)
+                if temp<diff_t:
+                    diff_t = temp
+            i+=1
         
-        if grabbed==1: # when the robot grabs the silver box, breaks out of the loop
-            break
-    return grabbed,pair_chk
-    
-"""    
-    
+    if diff_t<1.3*d_th: # checking whether the minimum distance of the silver tokens with the golden token is below a threshold
+        pair_gs = 1
+    return pair_gs
 
 
+def check_s_pair(dist_s, d_th):
+    """
+    function to check whether the silver token in pursuit is in a pair with a golden token.
+    to do that, first the shortest distance between the silver token and the golden tokens are calculated and if the shortest distance is smaller than a threshold, then the token is in pair
+    """
+    dist = 100
+    diff_t = -1
+    i = 1
+    pair_sg = 0
+    for token in R.see():
+        if token.dist < dist and token.info.marker_type is MARKER_TOKEN_GOLD:
+            if i==1:
+                diff_t = abs(token.dist-dist_s)
+            else:
+                temp = abs(token.dist-dist_s)
+                if temp<diff_t:
+                    diff_t = temp
+            i+=1
+    if diff_t<1.2*d_th:# checking whether the minimum distance of the golden tokens with the silver token is below a threshold
+        pair_sg = 1
+    return pair_sg
 
-pair_chk = 0 
-dropped = 0
+
+pair_chk = 0 #variable to track whether all the tokens are in pair
+dropped = 0 #variable to track whether silver token is dropped
 
 while 1:
     dist_s, rot_y_s = find_silver_token()
     dist_g, rot_y_g = find_golden_token()
+    if dist_s!=-1: # if silver token is detected, checking whether the token is pair
+        pair_sg = check_s_pair(dist_s, d_th)
+        if pair_sg == 1:# and dist_s>2*d_th: # if the golden token is in pair, rotate the robot and count the numbers to break once we find that all the tokens are in pair
+            print("The silver token is pair with a golden token, again checking")
+            turn(+10, 1)
+            pair_chk+=1
+            if pair_chk > 10:# breaking the loop once continously 10 rotations are done
+                print("All the tokens are in pairs, robot is stopping")
+                break
+            continue
     if dist_s==-1: # if no silver token is detected, we make the robot turn 
         print("I don't see any token!!")
         turn(+10, 1)
-    elif (dist_g!=-1 and (abs(dist_s-dist_g)<(1.3*d_th) and abs(rot_y_s-rot_y_g)<a_th)): # checking whether the silver token found is close to a golden tocken
-        print("The silver token is pair with a golden token, again checking")
-        turn(+10,1)
-        pair_chk+=1
-
 
     elif dist_s <d_th: # if we are close to the token, we try grab it.
         print("Found it!")
         if R.grab(): # if we grab the token, we move the robot forward and on the right, we release the token, and we go back to the initial position
+            pair_chk = 0 # reinitialising to zero
             print("Gotcha!")
             
             while 1: # loop for dropping the silver token
                 dist_g_m, rot_y_g_m = find_golden_token()
-                dist_s_m, rot_y_s_m = find_silver_token()
+                #dist_s_m, rot_y_s_m = find_silver_token()
+                if dist_g_m!=-1: # if golden token is detected, checking whether the token is in pair
+                    pair_gs = check_g_pair(dist_g_m, d_th)
+                    if pair_gs == 1 and dist_g_m>3*d_th: # the golden token is in pair, then rotating the robot
+                        print("The golden token is pair with a silver token, again checking")
+                        turn(+10, 1)
+                        continue
                 
                 if dist_g_m==-1: # if no golden token is detected, we make the robot turn 
                     print("I don't see any token!!")
                     turn(+10, 1)
-                elif (dist_g!=-1 and (abs(dist_s-dist_g)<d_th and abs(rot_y_s-rot_y_g)<a_th)): # checking whether the golden token found is close to a silver tocken
-                    print("The golden token is pair with a silver token, again checking")
-                    turn(+10,1)
                     
                 elif dist_g_m <(1.3*d_th): # if we are close to the golden token, we try to release the silver token.
                     print("Close to the golden token, releasing the silver token")
@@ -183,13 +185,13 @@ while 1:
                     break
 
                 elif -a_th<= rot_y_g_m <= a_th: # if the robot is well aligned with the token, we go forward
-                    print("Ah, that'll do.")
+                    #print("Ah, that'll do.")
                     drive(10, 0.5)
                 elif rot_y_g_m < -a_th: # if the robot is not well aligned with the token, we move it on the left or on the right
-                    print("Left a bit...")
+                    #print("Left a bit...")
                     turn(-2, 0.5)
                 elif rot_y_g_m > a_th:
-                    print("Right a bit...")
+                    #print("Right a bit...")
                     turn(+2, 0.5)
         else:
             print("Aww, I'm not close enough.")
@@ -205,3 +207,5 @@ while 1:
 
     if pair_chk == 10: # checking whether all the silver and golden tokens are in pairs
         break # breaking away from the loop
+
+#"""
